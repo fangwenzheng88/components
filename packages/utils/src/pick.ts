40487361b1e0f4b64270ed6d88c359b6ea1a229d
fangwenzheng <fangwenzheng@cestc.cn>
2023-09-01 15:47:38 +0800

    feat(utils): 新增pickTransform函数

--- packages/utils/src/pick.ts
+++ packages/utils/src/pick.ts
@@ -4,6 +4,7 @@ export function pick<T extends object, K extends keyof T>(source: T | undefined 
 
 /**
  * 根据参数类型返回相应的结果
+ * @category pick
  * @param source 源对象，可以为 null 或 undefined
  * @param fields 要选择的字段数组
  * @returns 选择的字段组成的新对象，如果源对象为空值，则返回一个空对象
