0ab79eccbc7e0653d2fde24c16fbded6737bb639
fangwenzheng <fangwenzheng@cestc.cn>
2023-10-18 16:36:03 +0800

    feat: update

---
+++ packages/hooks/src/__tests__/useSelectOptions.test.ts
@@ -0,0 +1,43 @@
+import { test, expect, describe } from 'vitest'
+import { useSelectOptions } from '../useSelectOptions'
+
+describe('useSelectOptions', () => {
+  test('入参为普通options数组', () => {
+    const { options, getOptions, getLabels } = useSelectOptions([{ label: 'label-1', value: 1 }])
+    expect(options.value).toMatchSnapshot()
+    expect(getOptions(1)).toEqual({ label: 'label-1', value: 1 })
+    expect(getLabels(1)).toBe('label-1')
+  })
+
+  test('入参为options数组，value为对象类型', () => {
+    const { options, getOptions, getLabels } = useSelectOptions([{ label: 'label-1', value: { value: 1 } }])
+    expect(options.value).toMatchSnapshot()
+    expect(getOptions(1)).toEqual({ label: 'label-1', value: { value: 1 } })
+    expect(getOptions({ value: 1 })).toEqual({ label: 'label-1', value: { value: 1 } })
+    expect(getLabels({ value: 1 })).toBe('label-1')
+    expect(getLabels(1)).toBe('label-1')
+  })
+
+  test('入参为options数组，value为对象类型，自定义valueKey', () => {
+    const { options, getOptions, getLabels } = useSelectOptions([{ label: 'label-1', value: { valueKey: 1 } }], { valueKey: 'valueKey' })
+    expect(options.value).toMatchSnapshot()
+    expect(getOptions(1)).toEqual({ label: 'label-1', value: { valueKey: 1 } })
+    expect(getOptions({ valueKey: 1 })).toEqual({ label: 'label-1', value: { valueKey: 1 } })
+    expect(getLabels({ valueKey: 1 })).toBe('label-1')
+    expect(getLabels(1)).toBe('label-1')
+  })
+
+  test('入参为函数', () => {
+    const { options, getOptions, getLabels } = useSelectOptions(
+      () => {
+        return Promise.resolve([{ label: 'label-1', value: 1 }])
+      },
+      { immediate: true }
+    )
+    setTimeout(() => {
+      expect(options.value).toMatchSnapshot()
+      expect(getOptions(1)).toEqual({ label: 'label-1', value: 1 })
+      expect(getLabels(1)).toBe('label-1')
+    }, 1000)
+  })
+})
--- packages/hooks/src/useSelectOptions.ts
+++ packages/hooks/src/useSelectOptions.ts
@@ -14,10 +14,35 @@ type Config = {
 }
 
 /**
- * a-select组件options数据的hooks工具
- * @param fun options数据或者一个返回options数据的接口函数
- * @param _config
- * @returns
+ * a-select 组件 options 数据的 hooks 工具
+ *
+ * @param fun options 数据或者一个返回 options 数据的接口函数
+ * @param _config 配置
+ * @param _config.valueKey 当 option 的 value 为 object 类型时，需要指定唯一标识的字段名，默认：`value`
+ * @param _config.immediate 是否立即调用一次 loadOptions，默认：`false`
+ * @param _config.fallback 当 value 无匹配 option 时，option 的回退函数
+ *
+ * @example
+ * ```typescript
+ * const { options } = useSelectOptions([{ label:'label-1', value:'1' }])
+ *
+ * // 传入函数
+ * const { options, getOptions, getLabels } = useSelectOptions((params) => {
+ *    console.log('传入的参数：',params)
+ *    return Promise.resolve([{ label: 'label-1', value: 1 }])
+ * })
+ * loadOptions({type:1}) // 调用传入的函数
+ *
+ * // 传入函数，并立即调用loadOptions()
+ * const { options, getOptions, getLabels } = useSelectOptions(() => {
+ *    return Promise.resolve([{ label: 'label-1', value: 1 }])
+ * },{immediate:true})
+ *
+ * // value为object类型
+ * const { options, getOptions, getLabels } = useSelectOptions([{ label: 'label-1', value: {value:1} }])
+ * getLabels(1) // label-1
+ *
+ * ```
  */
 export function useSelectOptions(fun?: FetchOptionsFunction | SelectOption[], _config?: Partial<Config>) {
   const defaultConfig: Config = { valueKey: 'value', immediate: false }
@@ -159,9 +184,18 @@ export function useSelectOptions(fun?: FetchOptionsFunction | SelectOption[], _c
   return {
     loading,
     options,
+    /**
+     * 加载options，传入immediate=true，会在初始化的时候调用一次
+     */
     loadOptions,
     setOptions,
+    /**
+     * 根据value获取对应的option，option会从之前缓存的options选项中获取，缓存的选项没有会调用传入的fallback函数并返回fallback返回的回退option，如果还没有返回undefined
+     */
     getOptions,
+    /**
+     * 根据value获取对应的label，label会从之前缓存的options选项中获取，缓存的选项没有会调用传入的fallback函数并返回fallback返回的回退option.label，如果还没有返回undefined
+     */
     getLabels,
     fallbackOption
   }
